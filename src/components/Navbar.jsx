89d369ce1465fc11b42b880f2bd153f5b90c129f
Sofia Ex <dev.exarchou@outlook.com>
2024-10-07 22:03:00 +0200

    design improvements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,40 +10,33 @@ export const NavBar = () => {
     } = useCategories();
 
     return (
-        <nav className="py-1 bg-cyan-600 top-0 sticky relative inset-0 w-full h-full object-cover" alt="Logo">
-            <div className="z-10 flex justify-between w-full  px-4 md:px-8 relative">
-                <Link to="/" className="mr-4 hover:text-lime-300 text-lg text-white transition-colors duration-300">
-                    <img src="/logo.png" className='h-12' />
-                </Link>
+        <nav className="z-10 flex justify-between w-full  px-4 md:px-8 relative py-1 bg-cyan-600 top-0 sticky relative inset-0 w-full h-full object-cover" alt="Logo">
+            <Link to="/" className="mr-4 hover:text-lime-300 text-lg text-white transition-colors duration-300">
+                <img src="/logo.png" className='h-12' />
+            </Link>
 
-                <div className="p-5 flex-1 flex justify-center">
-                    <select
-                        // defaultValue={selectedCategory === undefined ? "Pick a category" : selectedCategory}
-                        defaultValue={selectedCategory ?? "Pick a category"}
-                        onChange={({ target }) => setSelectedCategory(target.value)}
-                        className="select rounded-md  lg:w-1/3  w-full max-w-xs"
-                    >
-                        <option
-                            disabled
-                            key="default-option"
-                        >
-                            Pick a category
-                        </option>
+            <select
+                className="select my-1 rounded-md w-full max-w-xs"
+                defaultValue={selectedCategory ?? "Pick a category"}
+                onChange={({ target }) => setSelectedCategory(target.value)}
+            >
+                <option
+                    disabled
+                    key="default-option"
+                >
+                    Pick a category
+                </option>
 
-                        {categories.map(category => (
-                            <option key={category}>
-                                {category}
-                            </option>
-                        ))}
-                    </select>
-                </div>
+                {categories.map(category => (
+                    <option key={category}>
+                        {category}
+                    </option>
+                ))}
+            </select>
 
-                <Link to="/basket" className="hover:text-lime-300 text-lg text-white transition-colors duration-300">Your Basket</Link>
-            </div>
-            <div className='flex items-end justify-end px-4'>
-                <img src="./src/assets/images/shopping-basket.png" className="h-10" >
-                </img>
-            </div>
+            <Link to="/basket" className="hover:text-lime-300 text-lg text-white transition-colors duration-300">
+                <img src="./src/assets/images/shopping-basket.png" className="h-10" />
+            </Link>
         </nav>
     )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,8 +8,9 @@ const HomePage = () => {
 
     return (
         <div className="text-center text-slate-800 p-8 min-h-screen p-6 bg-slate-100">
-            <h1 className="text-4xl text-center p-5 mb-10 font-bold" >Your Amazing e-shopping experience awaits!</h1>
-            <h1 className="text-3xl font-bold text-center p-2 mb-10">All Products</h1>
+            <h1 className="text-4xl text-center mb-10 font-bold">
+                Your Amazing e-shopping experience awaits!
+            </h1>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                 {selectedCategory === undefined
