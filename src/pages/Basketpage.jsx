3796619f6601ba0b37d70abf466a160806f97969
Sofia Ex <dev.exarchou@outlook.com>
2024-10-08 00:35:01 +0200

    create total amount aside section

--- src/pages/Basketpage.jsx
+++ src/pages/Basketpage.jsx
@@ -2,6 +2,7 @@ import { ProductCardWithQuantity } from "../components/ProductCardWithQuantity";
 import { useProducts } from "../context/ProductContext";
 import { useBasket } from "../context/BasketContext";
 import { useEffect } from "react";
+import { BasketNavView } from "../components/BasketNavView";
 
 const BasketPage = () => {
     const products = useProducts();
@@ -22,7 +23,6 @@ const BasketPage = () => {
                         ...bItem,
                         ...(products.find(({ id }) => id === bItem.id))
                     }))
-
                     .map(product => (
                         <ProductCardWithQuantity
                             key={product.id}
@@ -33,8 +33,22 @@ const BasketPage = () => {
                     ))}
             </div>
 
-            <aside className="flex justify-end border border-cyan-400">
-                <img src="./src/assets/images/shopping-basket.png" className="h-20" />
+            <aside className="flex justify-end items-center border-4 border-cyan-500
+                            h-36 w-36 fixed top-[66%] right-0 transform -translate-y-1/2">
+                <img src="./src/assets/images/shopping-basket.png" className="h-25 m-8" />
+                {/* <p> Total: {BasketNavView}€ </p> */}
+                <p className="items-center justify-center flex">
+                    Total:
+                    {basketItems
+                        .map(bItem => ({
+                            ...bItem,
+                            ...(products.find(({ id }) => id === bItem.id))
+                        }))
+                        .map(({ price, quantity }) => price * quantity)
+                        .reduce((accumulator, currentValue) => (accumulator + currentValue), 0)
+                        .toFixed(2)
+                    }
+                </p>
             </aside>
         </div >
 
