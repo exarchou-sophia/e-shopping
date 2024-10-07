527ce8d04b1698c411d1f54a3c2cacaefeb74192
Sofia Ex <dev.exarchou@outlook.com>
2024-10-07 08:28:19 +0200

    setting colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,16 @@ export default {
         extend: {},
     },
     plugins: ["daisyui"],
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    primary: colors.cyan["600"],
+                    secondary: colors.slate["500"]
+                    accent: colors.amber["600"]
+                    neutral: colors.stone["300"]
+                },
+            },
+        ],
+    },
 }
