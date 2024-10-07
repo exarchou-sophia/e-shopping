ae41aae3b3098ce4ed1304ba9fdb5984bac581a2
Sofia Ex <dev.exarchou@outlook.com>
2024-10-07 23:54:34 +0200

    refresh basket view

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,36 +1,47 @@
 import { Button } from "./Button";
 import { addBasketItemToStorage, decrementBasketQuantity, removeItemFromStorageById } from "../storage/basket";
+import { useBasket } from "../context/BasketContext";
 
-export const ProductCard = props => (
-    <div className="card w-full rounded-lg lg:w-96 bg-base-100 h-full flex flex-col shadow-xl">
-        <img src={props.image} alt={props.name} className="h-48 w-full object-contain" />
+export const ProductCard = props => {
+    const { refreshBasketItem } = useBasket();
 
-        <div className="card-body">
-            <h2 className="card-title">{props.title}</h2>
-            <span className="text-lg font-semibold p-2 text-cyan-600">{`${props.price}€`}</span>
-            <p>{props.description}</p>
+    return (
+        <div className="card w-full rounded-lg lg:w-96 bg-base-100 h-full flex flex-col shadow-xl">
+            <img src={props.image} alt={props.name} className="h-48 w-full object-contain" />
 
-            <div className="card-actions justify-end">
-                <Button
-                    className="px-4"
-                    title=" + "
-                    onClicked={() => {
-                        console.log("item added", props)
-                        addBasketItemToStorage({
-                            id: props.id
-                        })
-                    }}
-                />
+            <div className="card-body">
+                <h2 className="card-title">
+                    {props.title}
+                </h2>
+                <span className="text-lg font-semibold p-2 text-cyan-600">
+                    {`${props.price}€`}
+                </span>
+                <p>{props.description}</p>
 
-                <Button
-                    className="px-4"
-                    title=" - "
-                    onClicked={() => {
-                        console.log("item decremented", props)
-                        decrementBasketQuantity(props.id)
-                    }}
-                />
+                <div className="card-actions justify-end">
+                    <Button
+                        className="px-4"
+                        title=" + "
+                        onClicked={() => {
+                            console.log("item added", props)
+                            addBasketItemToStorage({
+                                id: props.id
+                            })
+                            refreshBasketItem()
+                        }}
+                    />
+
+                    <Button
+                        className="px-4"
+                        title=" - "
+                        onClicked={() => {
+                            console.log("item decremented", props)
+                            decrementBasketQuantity(props.id)
+                            refreshBasketItem()
+                        }}
+                    />
+                </div>
             </div>
         </div>
-    </div>
-);
+    )
+}
